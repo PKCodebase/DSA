b0fd6f1dc65a7e06501615feab572496884a355f
PKCodebase <kaushikprasad1659@gmail.com>
2025-03-13 20:47:17 +0530

    Add new Program

---
+++ Sorting/src/com/sorting/BubbleSort.java
@@ -0,0 +1,30 @@
+package com.sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+
+        int n = arr.length;
+
+        for(int i=0;i<n;i++){
+//            System.out.println("Round"+i);
+            for(int j=0;j<n-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+//            System.out.println(Arrays.toString(arr));
+        }
+
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,5,3,4,4,5,6,5,8,9};
+        System.out.println(Arrays.toString(arr));
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
