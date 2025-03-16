c1ee8681a669e286e3d11c5bc9e48569ce70c57f
PKCodebase <kaushikprasad1659@gmail.com>
2025-03-16 17:23:40 +0530

    SelectionSort

---
+++ Sorting/src/com/sorting/SelectionSort.java
@@ -0,0 +1,27 @@
+package com.sorting;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        int temp=0;
+        int min ;
+        for(int i=0;i<arr.length;i++ ){
+          min =i;
+          for(int j = i+1;j<arr.length;j++){
+              if(arr[j]<arr[min])
+              {
+                  min = j;
+              }
+
+
+          }
+            temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+
+      }
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]+" ");
+        }
+    }
+}
