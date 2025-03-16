293faab8696e461da4b9d65aadbdd647402dbb09
PKCodebase <kaushikprasad1659@gmail.com>
2025-03-16 18:25:19 +0530

    InsertionSort

---
+++ Sorting/src/com/sorting/InsertionSort.java
@@ -0,0 +1,20 @@
+package com.sorting;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = { 12, 11, 13, 5, 6 };
+        int temp,j;
+        for(int i=1;i<arr.length;i++){
+            temp = arr[i];
+            j=i-1;
+            while(j>=0 && arr[j]>temp){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = temp;
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]+" ");
+        }
+    }
+}
