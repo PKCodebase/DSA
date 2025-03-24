e23bfbc070436012bcb1966b765d6b6441764511
PKCodebase <kaushikprasad1659@gmail.com>
2025-03-24 17:23:11 +0530

    HeapSort

--- Sorting/src/heap/HeapSort.java
+++ Sorting/src/heap/HeapSort.java
@@ -1,24 +1,46 @@
 package heap;
 
 public class HeapSort {
-    public static void main(String[] args) {
-        int[] arr = {5, 4, 3, 2, 1};
-        int temp=0;
-        int max ;
-        for(int i=0;i<arr.length;i++ ){
-            max =i;
-            for(int j = i+1;j<arr.length;j++){
-                if(arr[j]>arr[max])
-                {
-                    max = j;
-                }
-            }
-            temp = arr[i];
-            arr[i] = arr[max];
-            arr[max] = temp;
+    public static void heapiFy(int arr[],int n,int i){
+        int maxIndex = i;
+        int left = 2*i+1;
+        int right = 2*i+2;
+        if(left < n && arr[left] > arr[maxIndex]){
+            maxIndex = left;
         }
+        if(right < n && arr[right] > arr[maxIndex]){
+            maxIndex = right;
+        }
+        if(maxIndex != i){
+            int temp = arr[i];
+            arr[i] = arr[maxIndex];
+            arr[maxIndex] = temp;
+            heapiFy(arr,n,maxIndex);
+        }
+    }
+    public static void heapSort(int arr[]){
+        int n = arr.length;
+        for(int i=n/2-1;i>=0;i--){
+            heapiFy(arr, n, i);
+        }
+        for(int i=n-1;i>=0;i--){
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapiFy(arr, i, 0);
+        }
+    }
+    public static void buildMaxHeap(int arr[]){
+        int n = arr.length;
+        for(int i=n/2-1;i>=0;i--){
+            heapiFy(arr, n, i);
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {10, 80, 30, 90, 40, 50,12,6,19, 70};
+        heapSort(arr);
         for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]+" ");
+            System.out.print(arr[i]+" ");
         }
     }
 }
