c9a55d2a93183c6f2a26190c78dda31d981b1f4c
PKCodebase <kaushikprasad1659@gmail.com>
2025-04-03 17:52:51 +0530

    Add more labs

---
+++ Array/src/com/array/MaximumTriplet_I.java
@@ -0,0 +1,20 @@
+package com.array;
+
+public class MaximumTriplet_I {
+    public long maximumTripletValue(int[] nums) {
+        int n = nums.length;
+        long maxTripletValue =0,maxFirstElement  =0,maxDifference =0;
+        for(int k = 0;k<n;k++){
+            maxTripletValue = Math.max(maxTripletValue,maxDifference*nums[k]);
+            maxDifference = Math.max(maxDifference,maxFirstElement-nums[k]);
+            maxFirstElement = Math.max(maxFirstElement,nums[k]);
+        }
+
+        return maxTripletValue;
+    }
+    public static void main(String[] args) {
+        MaximumTriplet_I m=new MaximumTriplet_I();
+        int nums[]={1,2,3};
+        System.out.println(m.maximumTripletValue(nums));
+    }
+}
