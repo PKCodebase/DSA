a258f7a28a81b1fce716e9a12c2466b8ce620a11
PKCodebase <kaushikprasad1659@gmail.com>
2025-04-07 16:42:36 +0530

    MissingNumber

---
+++ Searching/src/searching/MissingNumber.java
@@ -0,0 +1,17 @@
+package searching;
+
+public class MissingNumber {
+    public static int missingNumber(int[] nums) {
+       int n = nums.length;
+        int totalSum = n* (n+1)/2;
+        int currentSum = 0;
+        for(int i=0;i<n;i++){
+            currentSum += nums[i];
+        }
+        return totalSum - currentSum;
+    }
+    public static void main(String[] args) {
+        int[] nums = {3,0,1};
+        System.out.println(missingNumber(nums));
+    }
+}
