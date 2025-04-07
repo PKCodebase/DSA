0e4e313269a125505611333f70ad4a5b4adf4afb
PKCodebase <kaushikprasad1659@gmail.com>
2025-04-07 16:58:16 +0530

    Sqrt

---
+++ Searching/src/searching/Sqrt.java
@@ -0,0 +1,33 @@
+package searching;
+
+public class Sqrt {
+    public static int mySqrt(int num){
+
+        if(num<2){
+            return num;
+        }
+        long start = 2;
+        long end = num/2;
+        long result = 0;
+        while(start<=end){
+            long mid = (start + end)/2;
+            result = mid * mid ;
+
+            if(result == num){
+                return (int) mid;
+            }
+            else if(result<num){
+                start = mid + 1;
+            }else if (result>num){
+                end =  mid -1;
+            }
+
+        }
+        return (int) end;
+
+    }
+    public static void main(String[] args) {
+        int num = 8;
+        System.out.println(mySqrt(num));
+    }
+}
