8fd0be544b23319e25834a9571737eda048fd846
PKCodebase <kaushikprasad1659@gmail.com>
2025-04-07 17:59:14 +0530

    Binary Search

---
+++ Searching/src/searching/FindDescendingIndex.java
@@ -0,0 +1,31 @@
+package searching;
+
+public class FindDescendingIndex {
+    public static void findIndex(int nums[],int target){
+
+        int start = 0;
+        int end = nums.length-1;
+        int ans = -1;
+        while (start<=end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] == target){
+                ans = mid;
+                break;
+            }else if(nums[mid]<target){
+                end = mid- 1;
+            }else{
+                start = mid+1;
+            }
+        }
+        if(ans == -1){
+            System.out.println("Element Not Found");
+        }else{
+            System.out.println("Element  Found at Index : " + ans);
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = {90,89,67,45,23,6,5,4,3,2,1};
+        int target = 5;
+        findIndex(nums,target);
+    }
+}
---
+++ Searching/src/searching/FindIndex.java
@@ -0,0 +1,36 @@
+package searching;
+
+public class FindIndex {
+    public static void findIndex(int[] arr, int target) {
+
+        int n= arr.length;
+     int start = 0;
+     int end = n-1;
+     int ans = -1;
+
+
+
+     while(start<=end){
+         int mid = start + (end - start)/2;
+         if(arr[mid] == target){
+             ans = mid;
+             break;
+         }else if(arr[mid]<target){
+             start = mid + 1;
+
+         }else{
+             end = mid-1;
+         }
+     }
+     if(ans == -1){
+         System.out.println("Element Not Found");
+     }else{
+         System.out.println("Element  Found at Index : " + ans);
+     }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,23,45,67,89,90};
+        int target = 5;
+        findIndex(arr,target);
+    }
+}
