055b0f144e38bd3ebb44897ef3d2bc97b7ff5299
PKCodebase <kaushikprasad1659@gmail.com>
2025-04-17 16:30:59 +0530

    CountEqualAndDivisiplePair

---
+++ Array/src/com/array/CountEqualAndDivisiplePair.java
@@ -0,0 +1,22 @@
+package com.array;
+
+public class CountEqualAndDivisiplePair { public int countPairs(int[] nums, int k) {
+    int n = nums.length;
+    int count=0;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(nums[i]==nums[j] && (i*j)%k==0){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+public static void main(String[] args) {
+    CountEqualAndDivisiplePair c = new CountEqualAndDivisiplePair();
+    int nums[] = {3, 1, 2, 2, 2, 1, 3};
+    int k = 2;
+    int ans = c.countPairs(nums, k);
+    System.out.println(ans);
+}
+}
