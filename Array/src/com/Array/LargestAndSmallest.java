c605848968ccc8836f9c7f5fc8d52d95a54237fe
Kaushik Prasad <147363330+PKCodebase@users.noreply.github.com>
2025-08-21 09:53:56 +0530

    Update LargestAndSmallest.java

--- Array/src/com/Array/LargestAndSmallest.java
+++ Array/src/com/Array/LargestAndSmallest.java
@@ -20,8 +20,7 @@ public class LargestAndSmallest {
         }
         System.out.println("Largest number is: "+max);
         System.out.println("Smallest number is: "+min);
-
-    }
+}
     public static void main(String[] args) {
         int nums[] = {1,2,3,4,5,6,7,8,9};
         largestAndSamllest(nums);
